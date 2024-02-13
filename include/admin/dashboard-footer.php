a1ee6925fcf18e4508b623a917f22593fb0e8f72
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2024-02-13 21:27:03 +0530

    admin dashboard update with add tour

---
+++ include/admin/dashboard-footer.php
@@ -0,0 +1,16 @@
+<div class="dashboard-footer" style="width: 100vw;">
+    <div class="copyright-area">
+        <p>Copyright 2023 <a href="#">Triprex</a> | Design By <a href="#">Egens Lab</a></p>
+    </div>
+    <ul class="footer-menu-list">
+        <li>
+            <a href="#">About Us</a>
+        </li>
+        <li>
+            <a href="#">Reviews</a>
+        </li>
+        <li>
+            <a href="#">Terms & Conditions</a>
+        </li>
+    </ul>
+</div>
---
+++ libs/connection.php
@@ -0,0 +1,25 @@
+<?php
+class Database
+{
+
+    public static $connection;
+
+    public static function setUpConnection()
+    {
+        if (!isset(Database::$connection)) {
+            Database::$connection = new mysqli("localhost", "root", "sulochana123", "travel_site_db", "3306");
+        }
+    }
+
+    public static function iud($q)
+    {
+        Database::setUpConnection();
+        Database::$connection->query($q);
+    }
+
+    public static function search($q)
+    {
+        Database::setUpConnection();
+        return Database::$connection->query($q);
+    }
+}
