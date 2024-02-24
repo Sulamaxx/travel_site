3ecca35a27ba8e9bdedb52ead8838a2658586e16
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2024-02-24 12:20:21 +0530

    database connect with cloud

--- libs/connection.php
+++ libs/connection.php
@@ -7,7 +7,7 @@ class Database
     public static function setUpConnection()
     {
         if (!isset(Database::$connection)) {
-            Database::$connection = new mysqli("localhost", "root", "sulochana123", "travel_site_db", "3306");
+            Database::$connection = new mysqli("62.72.50.204", "u795678648_travel", "Sulochana_sjr@123@", "u795678648_travel", "3306");
         }
     }
 
