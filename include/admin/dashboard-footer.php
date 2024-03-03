0aa668217920bc18a133fe1959909a9f15499920
Induwara Uthsara <induwarauthsara19@gmail.com>
2024-03-03 23:30:52 +0530

    Dashboard Footer

--- include/admin/dashboard-footer.php
+++ include/admin/dashboard-footer.php
@@ -1,16 +1,7 @@
 <div class="dashboard-footer" style="width: 100vw;">
     <div class="copyright-area">
-        <p>Copyright 2023 <a href="#">Travel Zoom Lanka</a> | Design By <a href="#">Egens Lab</a></p>
+        <p>©Copyright 2024 Travel Zoom Lanka - Designed by <a href="https://c-lento.com/">C_Lento</a></p>
     </div>
     <ul class="footer-menu-list">
-        <li>
-            <a href="#">About Us</a>
-        </li>
-        <li>
-            <a href="#">Reviews</a>
-        </li>
-        <li>
-            <a href="#">Terms & Conditions</a>
-        </li>
     </ul>
 </div>
