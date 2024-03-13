7b4f5667f1e434740ef9fe611c4e94262f81f8d1
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2024-03-13 14:12:01 +0530

    fix tours home button bug

--- index.php
+++ index.php
@@ -180,7 +180,7 @@ function limit_words($string, $word_limit)
                                                     <h5 style=" height: 1.5em; overflow: hidden;"><a href="package-details.php?id=<?= $data['id'] ?>"><?= $data['title'] ?></a></h5>
                                                     <p><?= limit_words($data['description'], 20) ?>...</p>
                                                 </div>
-                                                <div class="card-content-bottom position-absolute" style="margin-bottom: 0px;">
+                                                <div class="card-content-bottom">
                                                     <a href="package-details.php?id=<?= $data['id'] ?>" class="primary-btn2">Book a Tour
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                                                             <path d="M8.15624 10.2261L7.70276 12.3534L5.60722 18L6.85097 17.7928L12.6612 10.1948C13.4812 10.1662 14.2764 10.1222 14.9674 10.054C18.1643 9.73783 17.9985 8.99997 17.9985 8.99997C17.9985 8.99997 18.1643 8.26211 14.9674 7.94594C14.2764 7.87745 13.4811 7.8335 12.6611 7.80518L6.851 0.206972L5.60722 -5.41705e-07L7.70276 5.64663L8.15624 7.77386C7.0917 7.78979 6.37132 7.81403 6.37132 7.81403C6.37132 7.81403 4.90278 7.84793 2.63059 8.35988L0.778036 5.79016L0.000253424 5.79016L0.554115 8.91458C0.454429 8.94514 0.454429 9.05483 0.554115 9.08539L0.000253144 12.2098L0.778036 12.2098L2.63059 9.64035C4.90278 10.1523 6.37132 10.1857 6.37132 10.1857C6.37132 10.1857 7.0917 10.2102 8.15624 10.2261Z" />
